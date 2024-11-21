a1056dad355143753818fd6ac640cb898054b78b
Polina Nemechkina <136108608+qpollig@users.noreply.github.com>
2024-11-21 17:03:31 +0500

    Add files via upload

---
+++ Infrastructure/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TestBlazor.Infrastructure
+{
+    public static class StringExtensions
+    {
+        public static string FormatAsCounter(this string sourse)
+        {
+            if (sourse.Length != 4)
+            {
+                return sourse;
+            }
+
+            var second = sourse.Substring(0, 2);
+            var milliseconds = sourse.Substring(2, 2);
+            return second + ":" + milliseconds;
+        }
+    }
+}
